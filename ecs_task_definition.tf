ee3a9b179b07aab1bfc3a6dfe895d829f15cefd5
BhargavBharadwajM <133522637+BhargavBharadwajM@users.noreply.github.com>
2024-06-09 19:50:38 +0530

    Create ecs_task_definition.tf

---
+++ ecs_task_definition.tf
@@ -0,0 +1,22 @@
+resource "aws_ecs_task_definition" "hello_world_task" {
+  family                   = "hello-world-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "hello-world-container"
+      image     = "${aws_ecr_repository.hello_world_repo.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+}
